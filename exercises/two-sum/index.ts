9c215c006b370ccd3bdceb000aaeb5d1dbe06a41
Viacheslav Chykov <vyacheslav.chikov@gmail.com>
2021-03-15 20:38:27 +0200

    Task twoSum

---
+++ exercises/two-sum/index.ts
@@ -0,0 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function twoSum(nums: number[], target: number): number[] {
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++ ) {
+            if (nums[i] + nums[j] == target) {
+                return [i, j];
+            }
+        }
+    }
+    return [0, 0];
+}
