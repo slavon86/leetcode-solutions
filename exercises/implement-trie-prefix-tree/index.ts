d209825f36a715c6994e0dba1c78d961b58fd283
Viacheslav Chykov <vyacheslav.chikov@gmail.com>
2021-03-26 02:04:44 +0200

    Create file for task implement-trie-prefix-tree

---
+++ exercises/implement-trie-prefix-tree/index.ts
@@ -0,0 +1,15 @@
+export class Trie {
+    constructor() {}
+
+    insert(word: string): void {
+        return;
+    }
+
+    search(word: string): boolean {
+        return false;
+    }
+
+    startsWith(prefix: string): boolean {
+        return false;
+    }
+}
